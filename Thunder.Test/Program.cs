6807277514a4f5e0837fddf8a59fc3684b63dfd3
Bruce Thwaits <brucethwaits@gmail.com>
2015-09-30 12:54:22 +0200

    Added initial solution

---
+++ Thunder.Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thunder.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThunderClient.Net.Thunder thunder = new ThunderClient.Net.Thunder("[[server]]", "[[apikey]]", "[[secretkey]]");
+            int userCount = thunder.GetUserCount();
+            var msg = new
+            {
+                message = "test message"
+            };
+
+            int receivedMessage = thunder.SendMessageToChannel(msg, "test");
+
+            List<string> usersInChannel = thunder.GetUsersInChannel("test");
+
+            string username = "test";
+            if (usersInChannel.Count > 0)
+            {
+                username = usersInChannel.First();
+            }
+
+            int test = thunder.SendMessageToUser(msg, username);
+
+            bool test1 = thunder.IsUserOnline(username);
+            bool test2 = thunder.IsUserOnline("not-test");
+
+            thunder.DisconnectUser(username);
+            thunder.DisconnectUser("not-test");
+        }
+    }
+}
+
---
+++ ThunderClient.Net/Thunder.cs
@@ -0,0 +1,238 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThunderClient.Net
+{
+    public class Thunder
+    {
+        private string _server = "";
+        private string _apiKey = "";
+        private string _secretKey = "";
+        private IWebProxy _proxy = null;
+        private int? _timeout = null;
+
+        public Thunder(string server, string apiKey, string secretKey, IWebProxy proxy = null, int? timeout = null)
+        {
+            _server = server;
+            _apiKey = apiKey;
+            _secretKey = secretKey;
+            _proxy = proxy;
+            _timeout = timeout;
+        }
+
+        private static string SerializeContentAsJSON(object content)
+        {
+            Newtonsoft.Json.JsonSerializer jsonSerializer = new Newtonsoft.Json.JsonSerializer();
+            jsonSerializer.NullValueHandling = NullValueHandling.Ignore;
+
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+
+            jsonSerializer.Serialize(sw, content);
+            string output = sb.ToString();
+
+            output = output.Replace(@"\u0026", "&");
+
+            return output;
+        }
+
+        public int SendMessageToChannel(object message, string channel)
+        {
+            string url = string.Format("http://{0}/api/1.0.0/{1}/channels/{2}/", _server, _apiKey, channel);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-Thunder-Secret-Key", _secretKey);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            
+            if (_proxy != null)
+            {
+                request.Proxy = _proxy;
+            }
+
+            if (_timeout.HasValue)
+            {
+                request.Timeout = _timeout.Value;
+            }
+
+            string messageText = SerializeContentAsJSON(message);
+
+            byte[] toBytes = Encoding.ASCII.GetBytes(messageText);
+
+            Stream s = request.GetRequestStream();
+
+            s.Write(toBytes, 0, toBytes.Length);
+
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+
+            var reader = new StreamReader(response.GetResponseStream());
+            var objText = reader.ReadToEnd();
+
+            JObject o = JObject.Parse(objText);
+
+            return (int)o["count"];
+        }
+
+        public int GetUserCount()
+        {
+            string url = string.Format("http://{0}/api/1.0.0/{1}/users/", _server, _apiKey);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-Thunder-Secret-Key", _secretKey);
+            request.Method = "GET";
+
+            if (_proxy != null)
+            {
+                request.Proxy = _proxy;
+            }
+
+            if (_timeout.HasValue)
+            {
+                request.Timeout = _timeout.Value;
+            }
+
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+
+            var reader = new StreamReader(response.GetResponseStream());
+            var objText = reader.ReadToEnd();
+
+            JObject o = JObject.Parse(objText);
+
+            return (int)o["count"];
+        }
+
+        public List<string> GetUsersInChannel(string channel)
+        {
+            string url = string.Format("http://{0}/api/1.0.0/{1}/channels/{2}/", _server, _apiKey, channel);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-Thunder-Secret-Key", _secretKey);
+            request.Method = "GET";
+
+            if (_proxy != null)
+            {
+                request.Proxy = _proxy;
+            }
+
+            if (_timeout.HasValue)
+            {
+                request.Timeout = _timeout.Value;
+            }
+
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+
+            var reader = new StreamReader(response.GetResponseStream());
+            var objText = reader.ReadToEnd();
+
+            JObject o = JObject.Parse(objText);
+
+            return o["users"].Select(x => x.ToString()).ToList();
+        }
+
+        public int SendMessageToUser(object message, string user_id)
+        {
+            string url = string.Format("http://{0}/api/1.0.0/{1}/users/{2}/", _server, _apiKey, user_id);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-Thunder-Secret-Key", _secretKey);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+
+            if (_proxy != null)
+            {
+                request.Proxy = _proxy;
+            }
+
+            if (_timeout.HasValue)
+            {
+                request.Timeout = _timeout.Value;
+            }
+
+            string messageText = SerializeContentAsJSON(message);
+
+            byte[] toBytes = Encoding.ASCII.GetBytes(messageText);
+
+            Stream s = request.GetRequestStream();
+
+            s.Write(toBytes, 0, toBytes.Length);
+
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+
+            var reader = new StreamReader(response.GetResponseStream());
+            var objText = reader.ReadToEnd();
+
+            JObject o = JObject.Parse(objText);
+
+            return (int)o["count"];
+        }
+
+        public bool IsUserOnline(string user_id)
+        {
+            string url = string.Format("http://{0}/api/1.0.0/{1}/users/{2}/", _server, _apiKey, user_id);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-Thunder-Secret-Key", _secretKey);
+            request.Method = "GET";
+
+            if (_proxy != null)
+            {
+                request.Proxy = _proxy;
+            }
+
+            if (_timeout.HasValue)
+            {
+                request.Timeout = _timeout.Value;
+            }
+
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+
+            var reader = new StreamReader(response.GetResponseStream());
+            var objText = reader.ReadToEnd();
+
+            JObject o = JObject.Parse(objText);
+
+            return (bool)o["online"];
+        }
+
+        public void DisconnectUser(string user_id)
+        {
+            string url = string.Format("http://{0}/api/1.0.0/{1}/users/{2}/", _server, _apiKey, user_id);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Headers.Add("X-Thunder-Secret-Key", _secretKey);
+            request.Method = "DELETE";
+            request.ContentType = "application/json";
+
+            if (_proxy != null)
+            {
+                request.Proxy = _proxy;
+            }
+
+            if (_timeout.HasValue)
+            {
+                request.Timeout = _timeout.Value;
+            }
+
+            HttpWebResponse response = null;
+
+            response = (HttpWebResponse)request.GetResponse();
+        }
+    }
+}
